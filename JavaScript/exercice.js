ea8ab7d40b2c6f21de80f0b8cbd4ab2835afc019
benboura <bourabenraphael@gmail.com>
2024-10-15 08:58:21 +0300

    15/10

---
+++ JavaScript/exercice.js
@@ -0,0 +1,25 @@
+let totalLivres = 500; //étape 1: céez une variable `totaLivres` et l'initialiser à 500
+
+//étape 2: ajouter 50 livres avec l'opération `totalLivres +=50;`
+totalLivres +=50;
+
+//étape 3: enlever
+totalLivres-=10;
+
+//étape 4: rajouter 5
+totalLivres+= 5;
+
+// étape 5: utilisez console.log(totalLivres) pour vérifier le résultat.
+console.log(totalLivres);
+
+//étape 6: créer une nouvelle variable `affichagetTotalLivres` qui contient le résultat.
+let affichageTotalLivres = totalLivres;
+
+//étape 7: affivher le message avec `console.log...`
+console.log("Le nombres total de livres est :" + affichageTotalLivres)
+
+
+
+
+//CORRECTION
+
