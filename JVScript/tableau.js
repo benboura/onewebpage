749dbbcc9ffc4da9df5d3986727b95119c212694
benboura <bourabenraphael@gmail.com>
2024-10-15 08:45:45 +0300

    15/10

---
+++ JVScript/tableau.js
@@ -0,0 +1,29 @@
+/*
+les tableaux
+*/
+
+let monTableau = [ "Said", "Amina", "Bao"];
+
+// Je veux afficher les éléments du monTableau 
+
+console.log(monTableau[0]);
+console.log("j'ai trouvé ",monTableau[1])
+/* ("j'ai trouvé")..information type texte */
+
+
+
+// je declare mon "objet" (client)
+let client = {
+      nom:"Ali",
+      prenom:"Yanis",
+      numeroClient: 134,
+      adresse:"12 Rue de la vie - mayotte",
+      celibataire: false
+
+};
+
+//Avec console.log(), afficher le nom et le prenom du client...
+console.log(client.prenom)
+console.log(client.nom)
+console.log(client.adresse) 
+console.log(client.celibataire)
